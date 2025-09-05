00dfac3f3a0703f84a18c590bf0d912661df4c25
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-09-05 20:25:04 -0300

    feat: Add initial HTML and main.js for Electron application

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+// we import two modules from electron package
+// app module to control application life
+// BrowserWindow module to create and manage windows
+
 console.log("Hello, World! From Electron!");
+
+// create a new browser window with index.html
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+// when the app is ready, create the window
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
